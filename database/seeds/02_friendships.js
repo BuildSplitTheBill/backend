4fc5e6b05e6d982f2711ca2c6680f8eee4331609
Axolotl <samuelmachat@gmail.com>
2018-12-10 18:04:46 -0500

    add friendships seed data

--- database/seeds/02_friendships.js
+++ database/seeds/02_friendships.js
@@ -2,5 +2,12 @@ exports.seed = knex =>
   knex('friendships')
     .truncate()
     .then(() =>
-      knex('friendships').insert([{ id: 1, friend_1_id: 1, friend_2_id: 2 }])
+      knex('friendships').insert([
+        { id: 1, friend_1_id: 1, friend_2_id: 2 },
+        { id: 2, friend_1_id: 1, friend_2_id: 3 },
+        { id: 3, friend_1_id: 1, friend_2_id: 4 },
+        { id: 4, friend_1_id: 2, friend_2_id: 3 },
+        { id: 5, friend_1_id: 2, friend_2_id: 4 },
+        { id: 6, friend_1_id: 3, friend_2_id: 4 }
+      ])
     )
