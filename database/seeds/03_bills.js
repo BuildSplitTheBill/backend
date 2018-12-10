07aaecdd9d2a248db2758d2ef466ebba5741b6d7
Axolotl <samuelmachat@gmail.com>
2018-12-10 18:10:06 -0500

    add rest of seed data

--- database/seeds/03_bills.js
+++ database/seeds/03_bills.js
@@ -1,4 +1,9 @@
 exports.seed = knex =>
   knex('bills')
     .truncate()
-    .then(() => knex('bills').insert([{ id: 1, amount: 40, parties: 4 }]))
+    .then(() =>
+      knex('bills').insert([
+        { id: 1, amount: 40, parties: 4 },
+        { id: 2, amount: 200, parties: 3 }
+      ])
+    )
--- database/seeds/04_obligations.js
+++ database/seeds/04_obligations.js
@@ -10,6 +10,54 @@ exports.seed = knex =>
           is_owner: true,
           paid: true,
           date_paid: 'now'
+        },
+        {
+          id: 2,
+          bill_id: 1,
+          user_id: 2,
+          is_owner: false,
+          paid: true,
+          date_paid: 'now'
+        },
+        {
+          id: 3,
+          bill_id: 1,
+          user_id: 3,
+          is_owner: false,
+          paid: false,
+          date_paid: 'now'
+        },
+        {
+          id: 4,
+          bill_id: 1,
+          user_id: 4,
+          is_owner: false,
+          paid: false,
+          date_paid: 'now'
+        },
+        {
+          id: 5,
+          bill_id: 2,
+          user_id: 2,
+          is_owner: true,
+          paid: true,
+          date_paid: 'now'
+        },
+        {
+          id: 6,
+          bill_id: 2,
+          user_id: 3,
+          is_owner: false,
+          paid: false,
+          date_paid: 'now'
+        },
+        {
+          id: 7,
+          bill_id: 2,
+          user_id: 4,
+          is_owner: false,
+          paid: false,
+          date_paid: 'now'
         }
       ])
     )
