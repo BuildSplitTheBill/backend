cd8387d4921b725a7c87b36b3d1f8f50d39e0a2c
Axolotl <samuelmachat@gmail.com>
2018-12-11 15:22:02 -0500

    fix login controller

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -10,7 +10,7 @@ function login(req, res) {
     .then(user => {
       if (bcrypt.compareSync(password, user.password)) {
         req.session.name = username
-        res.status(200).json({ message: 'you may pass', token })
+        res.status(200).json({ message: 'you may pass' })
       } else {
         res.status(401).json({ message: 'you shall not pass' })
       }
