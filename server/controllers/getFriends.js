ae4c1b0d97324b39ddf1ebeec49b404dee596fed
Axolotl <samuelmachat@gmail.com>
2018-12-11 12:05:46 -0500

    add comment explaining getfriends database queries

--- server/controllers/getFriends.js
+++ server/controllers/getFriends.js
@@ -4,6 +4,14 @@ module.exports = async (req, res) => {
   const currentUser = req.session.name
 
   try {
+    // in our database we have two columns in the friends table
+    // friend_1_id and friend_2_id that designate connections
+    // because who might be in which column could be random
+    // we need to query each column of the table for instances
+    // of the user id, grab the corresponding user id (the friend)
+    // and lookup that user id in the users table. We then join these
+    // togethher to get a list of a users friends.
+
     const friendsColumn1 = await db('users as u1')
       .where({ 'u1.username': currentUser })
       .join('friendships as f', { 'u1.id': 'f.friend_1_id' })
